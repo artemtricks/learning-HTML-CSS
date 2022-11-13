5b0eb1d258464baa4e806ef8cfc04a160ef2342c
Ekaterina <grinina.e3654@yandex.ru>
2022-11-13 14:33:15 +0300

    add .

--- 13_javascript/1920/index.js
+++ 13_javascript/1920/index.js
@@ -82,27 +82,18 @@ document.querySelector(".header__btn").addEventListener("click", function() {
 
 // Accordion
 
-new Accordion('.container-first');
-
-// User options
-new Accordion('.container-second', {
-  duration: 400,
-  showMultiple: true,
-  onOpen: function(currentElement) {
-    console.log(currentElement);
-  }
+document.addEventListener("DOMContentLoaded", function() {
+  let acc = new Accordion('.faq__list', {
+  duration: 700,
+  elementClass:	'faq__item',
+  triggerClass: 'faq__btn',
+  panelClass: 'ac-panel',
+  showMultiple: false,
+  openOnInit: [0],
+    collapse: true
 });
-/*
-// Define several accordions with the same options (pass an array with selectors)
-new Accordion(['.container-first', '.container-second'], {});
+})
 
-// or pass an array with HTMLElements
-const accordions = Array.from(document.querySelectorAll('.accordion-container'));
-new Accordion(accordions, {});
-
-// Detach events
-const accordion = new Accordion('.container-first');
-accordion.detachEvents();*/
 
  
 
