2521262e7f81fc58b2a681c2795f1e67540d9b4d
Artem <artemtricks@yandex.ru>
2023-01-11 16:22:09 +0700

    stop-scroll

--- 17_bootstrap_grid/main.js
+++ 17_bootstrap_grid/main.js
@@ -1,15 +1,17 @@
 let burger = document.querySelector('.burger');
 let menu = document.querySelector('.nav');
-let menuLinks = menu.querySelectorAll(`${'.nav__close'}, ${'.nav__link'}`); 
-  
+let menuLinks = menu.querySelectorAll(`${'.nav__close'}, ${'.nav__link'}`);
+
 burger.addEventListener('click', function () {
     menu.classList.toggle('nav--active');
-    
+    document.body.classList.toggle('stop-scroll');
+
 });
-  
+
 menuLinks.forEach(function (el) {
     el.addEventListener('click', function () {
         menu.classList.remove('nav--active');
+        document.body.classList.remove('stop-scroll');
     })
-  
+
 });
